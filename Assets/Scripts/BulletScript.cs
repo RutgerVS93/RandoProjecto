41eba5c567122e978ba2fcd4b128a713803581d5
DESKTOP-FM7RFTU\R van Schoonderwalt <rutgerino@hotmail.com>
2017-11-30 23:40:02 +0100

    assets + guns toegevoegd

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -1,30 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Events;
 
 public class BulletScript : MonoBehaviour {
 
-    public float speed;
+    public float baseSpeed;
+    public GameObject impact;
 
-	void Start ()
-    {
-		
-	}
-	
-	void Update ()
+    [Space]
+    public bool speedUp;
+    public float _speedUp;
+
+    [Space]
+    public bool increaseSize;
+    public float _increaseSize;
+
+    void Update ()
     {
         Movement();
+        BulletMods();
+
+        Destroy(gameObject, 5f);
 	}
 
     void Movement()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        transform.Translate(Vector2.right * baseSpeed * Time.deltaTime);
+    }
+
+    public void BulletMods()
+    {
+        if (speedUp)
+        {
+            StartCoroutine(SpeedUpCoroutine());
+        }
+        if (increaseSize)
+        {
+            IncreaseSizeFunction();
+        }
+    }
+
+    IEnumerator SpeedUpCoroutine()
+    {
+        yield return new WaitForSeconds(.5f);
+        baseSpeed += _speedUp;
+    }
+
+    void IncreaseSizeFunction()
+    {
+        transform.localScale = new Vector3(transform.localScale.x + _increaseSize, transform.localScale.y + _increaseSize, transform.localScale.z + _increaseSize);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Ground"))
         {
+            GameObject impactClone = Instantiate(impact, transform.position, transform.rotation) as GameObject;
+            Destroy(impactClone, 2f);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -4,20 +4,64 @@ using UnityEngine;
 
 public class GunScript : MonoBehaviour {
 
-    public Vector3 mousePos;
-    public Vector3 gunPos;
-    public float angle;
+    Vector3 mousePos;
+    Vector3 gunPos;
+    float angle;
+    Rigidbody2D playerRb;
+    GameObject player;
 
     //Bullet
-    public GameObject bullet;
+    [Space]
+    public GameObject[] bullets;
+    public int bulletNumber;
+
+    [Space]
     public Transform firePoint;
     public float coolDown;
     public float timeStamp;
-	
-	void Update ()
+    public float knockBackForce;
+
+    public enum BulletMod
+    {
+        SpeedUp,
+        ScaleUp,
+        RapidFire,
+        SplitShot
+    }
+    public BulletMod bulletMod;
+
+    private void Start()
+    {
+        playerRb = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+    }
+
+    void Update ()
     {
         Spin();
-        Fire();
+        BulletVisual();
+
+        if (bulletMod == BulletMod.SplitShot)
+        {
+            StartCoroutine(SplitShot());
+        }
+        else if(bulletMod == BulletMod.RapidFire)
+        {
+            StartCoroutine(RapidFireShot());
+        }
+        else
+        {
+            Fire();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            bulletMod += 1;
+            if ((int)bulletMod > bullets.Length - 1)
+            {
+                bulletMod = 0;
+            }
+        }
+
     }
 
     void Spin()
@@ -34,9 +78,71 @@ public class GunScript : MonoBehaviour {
     void Fire()
     {
         if (Input.GetMouseButton(0) && timeStamp <= Time.time)
-        {            
-            GameObject bulletClone = Instantiate(bullet, firePoint.position, firePoint.rotation);
+        {         
+            GameObject bulletClone = Instantiate(bullets[bulletNumber], firePoint.position, firePoint.rotation);
             timeStamp = Time.time + coolDown;
+            KnockBack(knockBackForce);
+        }
+    }
+
+    IEnumerator SplitShot()
+    {        
+        if (Input.GetMouseButton(0) && timeStamp <= Time.time)
+        {
+            float bulletRotation = -20;
+            for (int i = 1; i < 4; i++)
+            {
+                GameObject bulletClone = Instantiate(bullets[bulletNumber], firePoint.position, Quaternion.Euler(0, 0, angle + bulletRotation)) as GameObject;                    
+                bulletRotation += 20;
+            }
+            KnockBack(knockBackForce);
+            timeStamp = Time.time + coolDown;
+        }
+
+        yield return new WaitForEndOfFrame();
+    }
+
+    IEnumerator RapidFireShot()
+    {
+        if (Input.GetMouseButton(0) && timeStamp <= Time.time)
+        {
+            GameObject bulletClone = Instantiate(bullets[bulletNumber], firePoint.position, Quaternion.Euler(0, 0, angle + Random.Range(-10, 10)));
+            timeStamp = Time.time + coolDown;
+            KnockBack(knockBackForce);
+        }
+        yield return new WaitForEndOfFrame();
+    }
+
+    void KnockBack(float _knockBack)
+    {
+        Vector3 dir = Quaternion.AngleAxis(angle, Vector3.forward) * Vector3.right;
+        playerRb.AddForce(-_knockBack * dir, ForceMode2D.Impulse);
+    }
+
+    void BulletVisual()
+    {
+        switch (bulletMod)
+        {
+            case BulletMod.SpeedUp:
+                bulletNumber = 0;
+                coolDown = .1f;
+                knockBackForce = 0;
+                break;
+            case BulletMod.ScaleUp:
+                bulletNumber = 1;
+                coolDown = .5f;
+                knockBackForce = 20;
+                break;
+            case BulletMod.RapidFire:
+                bulletNumber = 2;
+                coolDown = .05f;
+                knockBackForce = 2;
+                break;
+            case BulletMod.SplitShot:
+                bulletNumber = 3;
+                coolDown = .3f;
+                knockBackForce = 10;
+                break;
         }
     }
 }
