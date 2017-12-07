32013fe3be2ab9781d42e9e4fb9b0ce7203095cf
Rutger van Schoonderwalt <Rutger.van.Schoonderwalt@hva.nl>
2017-12-07 14:49:35 +0100

    dingen2

--- Assets/Scripts/Boss/Attack1.cs
+++ Assets/Scripts/Boss/Attack1.cs
@@ -13,18 +13,33 @@ public class Attack1 : MonoBehaviour {
 
     public float numberOfProjectiles;
 
+    public bool fallingObjectsAttack;
+    public bool circleAttack;
+    public bool backAndForthAttack;
 
     void Start ()
     {
-        StartCoroutine(SpawnProjectiles());
-	}
+        if (fallingObjectsAttack)
+        {
+            StartCoroutine(FallingObject());
+        }
+        if (circleAttack)
+        {
+            StartCoroutine(CircleObjects());
+        }
+        if (backAndForthAttack)
+        {
+            StartCoroutine(BackAndForthObjects());
+        }
+    }
 	
 	void Update ()
     {
 		
 	}
 
-    IEnumerator SpawnProjectiles()
+    //Bulletscript speed op 0
+    IEnumerator FallingObject()
     {
         for (int i = 0; i < numberOfProjectiles; i++)
         {
@@ -33,12 +48,37 @@ public class Attack1 : MonoBehaviour {
             Destroy(projectileClone, 5f);
         }
     }
+    
+    //Gravity uitzetten of rb kinematic
+    IEnumerator CircleObjects()
+    {
+        float bulletRotation = 0;
+        for (int i = 0; i < numberOfProjectiles; i++)
+        {
+            GameObject projectileClone = Instantiate(projectile, transform.position, Quaternion.Euler(0, 0, transform.rotation.z + bulletRotation));
+            yield return new WaitForSeconds(.1f);
+            bulletRotation += 10;
+            Destroy(projectileClone, 5f);
+        }
+    }
+
+    //Gravity projectiles aanpassen
+    IEnumerator BackAndForthObjects()
+    {
+        for (int i = 0; i < numberOfProjectiles; i++)
+        {
+            float bulletRotation = Random.Range(0, 180);
+            GameObject projectileClone = Instantiate(projectile, transform.position, Quaternion.Euler(0, 0, bulletRotation));
+            yield return new WaitForSeconds(.1f);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             GameObject impactClone = Instantiate(impact, projectileClone.transform.position, projectileClone.transform.rotation);
+            Destroy(impactClone, 1f);
         }
     }
 }
